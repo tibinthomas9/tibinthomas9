d57288c78d2df3816b526db7832762c8a4cfcbe8
Tibin Thomas <tibinmutholy@gmail.com>
2024-01-23 12:51:02 -0500

    Create Why Protocols.d

---
+++ _posts/Why Protocols.d
@@ -0,0 +1,15 @@
+# Why use protocols in swift
+
+## To provide abstraction
+  To generalize common behaviour or data out into an abstract structure which enables reusability of code as well as maintainability 
+  in a safe manner(as swift ensures type conformance at compile time)
+  
+  Whats abstraction https://www.tonymarston.co.uk/php-mysql/abstraction.txt
+
+
+## To share implementations which are abstract behaviours 
+   https://developer.apple.com/documentation/swift/choosing-between-structures-and-classes
+      Use structures along with protocols to adopt behaviour by sharing implementations.
+
+## which in turn helps to enforce the SOLID principle and cleaner code
+    https://codeburst.io/solid-design-principle-using-swift-fa67443672b8
